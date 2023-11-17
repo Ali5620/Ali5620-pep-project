692b07b2f6bb62d5a3c5e39456372592a8292b27
aliashrafff <89399922+aliashrafff@users.noreply.github.com>
2023-11-17 13:36:27 +0530

    COMPLETED

---
+++ src/main/java/DAO/AccountDao.java
@@ -0,0 +1,91 @@
+package DAO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+import java.sql.Statement;
+import java.sql.Connection;
+
+
+
+public class AccountDao {  
+    public Account createAccount(Account account){
+        Connection con=ConnectionUtil.getConnection();
+try{
+        String sql="INSERT INTO account (username, password) VALUES (?,?)";
+        PreparedStatement ps=con.prepareStatement (sql, Statement. RETURN_GENERATED_KEYS); 
+         ps.setString(1, account.getUsername());
+         ps.setString(2, account.getPassword());
+         ps.executeUpdate();
+         ResultSet keyrs=ps.getGeneratedKeys();
+         if (keyrs.next()){
+        int generated_account_key= (int) keyrs.getInt("account_id");
+        return new Account (generated_account_key, account.getUsername(), account.getPassword());
+ }
+       }catch(SQLException e)
+{
+
+         System.out.println(e.getMessage());
+}
+          return null;   
+}
+
+//verify user login
+          public Account verifyAccount (Account account){
+            Connection con=ConnectionUtil.getConnection(); 
+try {
+             String sql="select*from account where username=?";
+            PreparedStatement ps=con.prepareStatement(sql);
+            ps.setString(1, account.getUsername());
+            ResultSet rs=ps.executeQuery();
+           while(rs.next())
+{
+            return new Account(rs.getInt("account_id"),rs.getString("username"),rs.getString("password"));
+
+}
+}catch(SQLException e)
+{
+    System.out.println(e.getMessage());
+}
+return null;
+}
+
+
+          //retrive account by username
+public Account getAccountByUsername(String name) { 
+Connection con=ConnectionUtil.getConnection(); try {
+String sql="select*from account where username=(?)"; 
+PreparedStatement ps=con.prepareStatement (sql); 
+ps.setString(1, name);
+ResultSet rs=ps.executeQuery();
+while(rs.next()){
+return new Account (rs.getInt("account_id"), rs.getString("username"),rs.getString("password"));
+
+} }
+catch (Exception e) {
+ System.out.println(e.getMessage());}
+return null;
+}
+
+public Account getAccountByUserId(int id){
+    Connection con=ConnectionUtil.getConnection();
+    try {
+        String sql="select * from account where username=(?)";
+        PreparedStatement ps=con.prepareStatement(sql);
+        ps.setInt(1,id);
+        ResultSet rs=ps.executeQuery();
+        while(rs.next())
+        {
+            Account account=new Account(rs.getInt("account_id"),rs.getString("username"),rs.getString("password"));
+            return account;
+        }
+
+    } catch (Exception e) {
+        System.out.println(e.getMessage()); 
+   }
+    return null;
+}}
